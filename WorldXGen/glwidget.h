b9b4d464ef5520ce04edca65daf5a287f56791bc
guillaumetremeau <tremeauguillaume@gmail.com>
2017-05-17 16:26:21 +0200

    Initialisation vue opengl

--- WorldXGen/glwidget.h
+++ WorldXGen/glwidget.h
@@ -1,17 +1,39 @@
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
-#include <QGLWidget>
 
-class GLWidget : public QGLWidget
+#include <QtGui/qopengl.h>
+#include <QtGui>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <gl/GLU.h>
+
+class GLWidget : public QOpenGLWidget
 {
     Q_OBJECT
 public:
     explicit GLWidget(QWidget *parent = 0);
+    ~GLWidget();
 
     void initializeGL();
     void paintGL();
-    void resizeGL(int w, int h);
+    void resizeGL(int width, int height);
+
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+
+private:
+    void rotateBy(int x, int y, int z);
+
+private:
+
+    // View settings
+    QPoint last_pos;
+    float distance;
+    qreal x_rot;
+    qreal y_rot;
+    qreal z_rot;
 };
 
 #endif // GLWIDGET_H
