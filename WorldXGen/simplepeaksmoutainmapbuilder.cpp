707cf1a5ce094022c0f87da98ef7f4297bf43c3d
Nicolas Jullien <nerpson.gaming@gmail.com>
2017-05-21 14:58:48 +0200

    Changed the generation algorithm. Fixed some errors in the h file.

--- WorldXGen/simplepeaksmoutainmapbuilder.cpp
+++ WorldXGen/simplepeaksmoutainmapbuilder.cpp
@@ -4,76 +4,65 @@ Map * SimplePeaksMoutainMapBuilder::build()
 {
     Map * generatedMap = new Map(this->sizeX, this->sizeY);
 
-    unsigned int centerXpos = this->sizeX / 2;
-    unsigned int centerYpos = this->sizeY / 2;
-
-    Point3D * centerPoint = generatedMap->getPoint(centerXpos, centerYpos);
-
-    float centerPosHeight = abs(Random::normalDistribution(this->seed->getValue()) * 4);
-    centerPoint->setZ(centerPosHeight);
-
     vector<Point3D *> points = *(generatedMap->getAllPoints());
 
-    unsigned int maxSize = max(this->sizeX, this->sizeY);
-
-    for (unsigned int radius = 1; radius < maxSize / 2; ++radius)
-    {
-        for (Point3D * point : points)
-        {
-            float distance = sqrt(
-                pow(point->getX() - centerPoint->getX(), 2) +
-                pow(point->getY() - centerPoint->getY(), 2)
-            );
-
-            if (radius - 1 < distance && distance <= radius)
-            {
-                float height = (float) max(0., centerPosHeight - distance * Random::normalDistribution(this->seed->getValue()) / 10.);
-                point->setZ(height);
-            }
-        }
-    }
+    for (Point3D * point : points) { point->setZ(getGaussianValue(point->getX(), point->getY())); }
 
     return generatedMap;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionParams(float gShiftX, float gShiftY, float gSizeX, float gSizeY)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionParams(float gShiftX, float gShiftY, float gSizeX, float gSizeY)
 {
     this->gaussianShiftX    = gShiftX;
     this->gaussianShiftY    = gShiftY;
     this->gaussianSizeX     = gSizeX;
     this->gaussianSizeY     = gSizeY;
+
+    return this;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionShift(float gShiftX, float gShiftY)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionShift(float gShiftX, float gShiftY)
 {
     this->gaussianShiftX = gShiftX;
     this->gaussianShiftY = gShiftY;
+
+    return this;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionShiftX(float gShiftX)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionShiftX(float gShiftX)
 {
     this->gaussianShiftX = gShiftX;
+
+    return this;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionShiftY(float gShiftY)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionShiftY(float gShiftY)
 {
     this->gaussianShiftY = gShiftY;
+
+    return this;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionSize(float gSizeX, float gSizeY)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionSize(float gSizeX, float gSizeY)
 {
     this->gaussianSizeX = gSizeX;
     this->gaussianSizeY = gSizeY;
+
+    return this;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionSizeX(float gSizeX)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionSizeX(float gSizeX)
 {
     this->gaussianSizeX = gSizeX;
+
+    return this;
 }
 
-void SimplePeaksMoutainMapBuilder::setGaussianFunctionSizeY(float gSizeY)
+SimplePeaksMoutainMapBuilder * SimplePeaksMoutainMapBuilder::setGaussianFunctionSizeY(float gSizeY)
 {
     this->gaussianSizeY = gSizeY;
+
+    return this;
 }
 
 float SimplePeaksMoutainMapBuilder::getGaussianValue(float x, float y)
